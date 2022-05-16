d55317002db9b6605b13d86e34865d3353a24fbd
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:15:20 -0400

    Agregando Proyecto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int rango;
+
+Console.WriteLine("Introduzca el limite de la tabla: ");
+rango = Convert.ToInt16(Console.ReadLine());
+
+for(int i=1; i<=rango; i++)
+{
+    for(int j = 0; j <= 10; j++)
+    Console.WriteLine(i + "x" + j + "=" + (i*j));
+    Console.WriteLine("");
+}
+Console.ReadKey();
